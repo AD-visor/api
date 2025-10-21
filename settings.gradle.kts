da3005552d4e8ade78bc1649e93d0802d1dc56b4
liverforpresent <dailynsteady@gmail.com>
2025-10-21 16:09:54 +0900

    chore: 멀티 모듈 설정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,31 @@
 rootProject.name = "api"
 
+// App
 include(":app")
+
+// Common
 include(":common")
-include(":iam")
-include(":subscription")
-include(":payment")
-include(":content")
+
+// Iam
+include(":iam:application")
+include(":iam:domain")
+include(":iam:infrastructure")
+include(":iam:presentation")
+
+// Content
+include(":content:application")
+include(":content:domain")
+include(":content:infrastructure")
+include(":content:presentation")
+
+// Subscription
+include(":subscription:application")
+include(":subscription:domain")
+include(":subscription:infrastructure")
+include(":subscription:presentation")
+
+// Payment
+include(":payment:application")
+include(":payment:domain")
+include(":payment:infrastructure")
+include(":payment:presentation")
