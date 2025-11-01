4f3b25f40b8e25b5acfc2229925f32a3a89f7d54
liverforpresent <dailynsteady@gmail.com>
2025-11-02 01:12:09 +0900

    chore: jpa 등록을 위해 infrastructure 패키지 스캔

---
+++ app/src/main/kotlin/com/advisor/api/app/config/JpaConfig.kt
@@ -0,0 +1,14 @@
+package com.advisor.api.app.config
+
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@Configuration
+@EntityScan(basePackages = [
+    "com.advisor.iam.infrastructure",
+])
+@EnableJpaRepositories(basePackages = [
+    "com.advisor.iam.infrastructure",
+])
+class JpaConfig
