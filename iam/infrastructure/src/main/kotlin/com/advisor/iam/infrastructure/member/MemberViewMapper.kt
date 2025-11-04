7710badc689c9089ff489bd2fd3dfdb7bdf2059d
liverforpresent <dailynsteady@gmail.com>
2025-11-05 00:08:10 +0900

    feat: MemberViewMapper(Mybatis) 구현

---
+++ iam/infrastructure/src/main/kotlin/com/advisor/iam/infrastructure/member/MemberViewMapper.kt
@@ -0,0 +1,14 @@
+package com.advisor.iam.infrastructure.member
+
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Param
+import org.apache.ibatis.annotations.Select
+import java.util.Optional
+
+@Mapper
+interface MemberViewMapper {
+    @Select("SELECT * FROM vw_member WHERE id = #{id}")
+    fun findById(
+        @Param("id") id: Long
+    ): Optional<MemberViewEntity>
+}
