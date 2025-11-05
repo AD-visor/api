d14192b6e6e43d3d5d707792178c9ea237bce963
liverforpresent <dailynsteady@gmail.com>
2025-11-05 20:38:20 +0900

    chore: MyBatis 버림

--- iam/infrastructure/src/main/kotlin/com/advisor/api/iam/infrastructure/auth/AuthReaderImpl.kt
+++ iam/infrastructure/src/main/kotlin/com/advisor/api/iam/infrastructure/auth/AuthReaderImpl.kt
@@ -5,16 +5,20 @@ import com.advisor.api.iam.domain.auth.AuthReader
 import com.advisor.api.iam.domain.auth.AuthView
 import org.springframework.stereotype.Repository
 
+
 @Repository
 class AuthReaderImpl(
-    private val authViewMapper: AuthViewMapper
+    //private val authViewMapper: AuthViewMapper
 ): AuthReader {
     override fun findByMemberId(memberId: Long): AuthView {
+        TODO()
+        /*
         val authEntity = authViewMapper.findByMemberId(memberId).orElseThrow{ CustomException(
             AuthInfrastructureExceptionCode.AUTH_NOT_FOUND,
             "[Auth] ${memberId}에 해당하는 유저의 인증 정보가 존재하지 않습니다."
         ) }
 
         return AuthViewEntity.toModel(authEntity)
+        */
     }
 }
--- iam/infrastructure/src/main/kotlin/com/advisor/api/iam/infrastructure/member/MemberReaderImpl.kt
+++ iam/infrastructure/src/main/kotlin/com/advisor/api/iam/infrastructure/member/MemberReaderImpl.kt
@@ -7,14 +7,18 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class MemberReaderImpl(
-    private val memberViewMapper: MemberViewMapper
+    //private val memberViewMapper: MemberViewMapper
 ): MemberReader {
     override fun findById(id: Long): MemberView{
+        TODO()
+        /*
         val memberEntity = memberViewMapper.findById(id).orElseThrow{ CustomException(
             MemberInfrastructureExceptionCode.MEMBER_NOT_FOUND,
             "[Member] ${id}에 해당하는 회원이 존재하지 않습니다."
         ) }
 
         return MemberViewEntity.toModel(memberEntity)
+
+        */
     }
 }
