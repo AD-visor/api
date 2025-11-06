86646f20be7f56c3856a7e7e931214064ad95651
liverforpresent <dailynsteady@gmail.com>
2025-11-06 22:29:54 +0900

    feat: Plan Jpa Entity 구현

---
+++ subscription/infrastructure/src/main/kotlin/com/advisor/api/subscription/infrastructure/plan/PlanEntity.kt
@@ -0,0 +1,73 @@
+package com.advisor.api.subscription.infrastructure.plan
+
+import com.advisor.api.common.core.domain.vo.Money
+import com.advisor.api.common.core.domain.vo.identifier.PlanId
+import com.advisor.api.subscription.domain.plan.Plan
+import com.advisor.api.subscription.domain.plan.PlanProps
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.Instant
+
+@Entity
+@Table(name = "plan")
+class PlanEntity(
+    @Id
+    val id: Long,
+
+    @Column
+    val name: String,
+
+    @Column
+    val monthlyLimit: Long,
+
+    @Column
+    val price: Float,
+
+    @Column
+    val description: String?,
+
+    @Column
+    val createdAt: Instant,
+
+    @Column
+    val updatedAt: Instant,
+
+    @Column
+    val isDeleted: Boolean,
+
+    @Column
+    val deletedAt: Instant?,
+) {
+    companion object {
+        fun fromDomain(domain: Plan): PlanEntity {
+            return PlanEntity(
+                id = domain.id.value,
+                name = domain.name,
+                monthlyLimit = domain.monthlyLimit,
+                price = domain.price.value,
+                description = domain.description,
+                createdAt = domain.createdAt,
+                updatedAt = domain.updatedAt,
+                isDeleted = domain.isDeleted,
+                deletedAt = domain.deletedAt
+            )
+        }
+    }
+
+    fun toDomain(): Plan {
+        val props = PlanProps(
+            name = name,
+            monthlyLimit = monthlyLimit,
+            price = Money.create(price),
+            description = description,
+            createdAt = createdAt,
+            updatedAt = updatedAt,
+            isDeleted = isDeleted,
+            deletedAt = deletedAt
+        )
+
+        return Plan.of(PlanId(id), props)
+    }
+}
