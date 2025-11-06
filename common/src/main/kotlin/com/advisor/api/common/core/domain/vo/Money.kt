658eebaceef1595621d0168510a65d91cb29782d
liverforpresent <dailynsteady@gmail.com>
2025-11-06 22:28:38 +0900

    feat: Money value object 구현

---
+++ common/src/main/kotlin/com/advisor/api/common/core/domain/vo/Money.kt
@@ -0,0 +1,22 @@
+package com.advisor.api.common.core.domain.vo
+
+class Money private constructor(val value: Float) {
+    companion object {
+        fun create(value: Float): Money {
+            val money = Money(value)
+            money.validate()
+
+            return money
+        }
+    }
+
+    fun validate() {
+        require(value >= 0.0) { "[Money] 돈은 음수일 수 없습니다." }
+    }
+
+    operator fun plus(other: Money): Money = Money(this.value + other.value)
+    operator fun minus(other: Money): Money = Money(this.value - other.value)
+
+    override fun equals(other: Any?): Boolean = other is Money && this.value == other.value
+    override fun hashCode(): Int = value.hashCode()
+}
