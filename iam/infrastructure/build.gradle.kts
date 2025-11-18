cd76ba2c39dc53095804d661f0d36de02ad3a42e
liverforpresent <dailynsteady@gmail.com>
2025-11-19 00:21:28 +0900

    chore: jwt dependency 설정

--- iam/infrastructure/build.gradle.kts
+++ iam/infrastructure/build.gradle.kts
@@ -4,6 +4,13 @@ dependencies {
 
     // Spring
     implementation("org.springframework:spring-context")
+    implementation("org.springframework.boot:spring-boot")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
 
     // Database
     compileOnly("jakarta.persistence:jakarta.persistence-api")
