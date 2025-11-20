a2a56571a3b28dfc54237953ab3f621796f6feb6
liverforpresent <dailynsteady@gmail.com>
2025-11-21 00:02:34 +0900

    chore: 의존성 수정(infrastructure, presentation -> outbound/inbound adapter, outbound/inbound port)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,20 +2,23 @@ apply(plugin = "org.springframework.boot")
 
 dependencies {
     implementation(project(":common"))
-    implementation(project(":iam:presentation"))
+
+    implementation(project(":iam:adapter:inbound"))
+    implementation(project(":iam:adapter:outbound"))
+    implementation(project(":iam:port:outbound"))
+
     implementation(project(":conversation:presentation"))
     implementation(project(":subscription:presentation"))
     implementation(project(":payment:presentation"))
 
-    implementation(project(":iam:infrastructure"))
     implementation(project(":conversation:infrastructure"))
     implementation(project(":subscription:infrastructure"))
     implementation(project(":payment:infrastructure"))
 
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
 
     // Database
     runtimeOnly("org.postgresql:postgresql")
--- iam/application/build.gradle.kts
+++ iam/application/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation(project(":common"))
     implementation(project(":iam:domain"))
+    implementation(project(":iam:port:outbound"))
 
     implementation("org.springframework:spring-context")
 }
