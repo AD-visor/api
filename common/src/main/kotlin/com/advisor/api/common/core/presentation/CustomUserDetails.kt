befb8134d53d7fafc0b5e7b697442dd840af6574
liverforpresent <dailynsteady@gmail.com>
2025-11-25 00:32:30 +0900

    feat: CustomUserDetails 구현

---
+++ common/src/main/kotlin/com/advisor/api/common/core/presentation/CustomUserDetails.kt
@@ -0,0 +1,29 @@
+package com.advisor.api.common.core.presentation
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class CustomUserDetails (
+    val id: Long,
+    private val username: String,
+    private val roles: List<String>
+) : UserDetails {
+    override fun getAuthorities(): Collection<GrantedAuthority> {
+        return roles.map { role -> SimpleGrantedAuthority(role) }
+    }
+
+    override fun getPassword(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUsername(): String = username
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
