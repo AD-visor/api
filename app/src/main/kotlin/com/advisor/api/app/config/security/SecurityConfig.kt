997fa90a4dc6768a681a6bf1d5475879cff7e9b9
liverforpresent <dailynsteady@gmail.com>
2025-11-25 00:30:16 +0900

    chore: SecurityConfig에 OAuth2Login 로직 적용

--- app/src/main/kotlin/com/advisor/api/app/config/security/SecurityConfig.kt
+++ app/src/main/kotlin/com/advisor/api/app/config/security/SecurityConfig.kt
@@ -3,9 +3,12 @@ package com.advisor.api.app.config.security
 import com.advisor.api.app.config.security.SecurityPathFilter.AUTH_PATHS
 import com.advisor.api.app.config.security.SecurityPathFilter.PUBLIC_PATHS
 import com.advisor.api.app.config.security.jwt.JwtAuthenticationFilter
+import com.advisor.api.iam.adapter.inbound.auth.oauth.CustomOAuth2UserService
+import com.advisor.api.iam.adapter.inbound.auth.oauth.OAuth2LoginSuccessHandler
 import com.advisor.api.iam.port.outbound.auth.AuthTokenPort
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -16,7 +19,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
-    private val authTokenPort: AuthTokenPort
+    private val authTokenPort: AuthTokenPort,
+    @Lazy private val customOAuth2UserService: CustomOAuth2UserService,
+    @Lazy private val oAuth2LoginSuccessHandler: OAuth2LoginSuccessHandler
 ) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -37,14 +42,10 @@ class SecurityConfig(
         }
 
         // OAuth2 로그인 설정
-        /*
         http.oauth2Login {
-            it.userInfoEndpoint {
-            }
-            //it.successHandler()
+            it.userInfoEndpoint { u ->  u.userService(customOAuth2UserService) }
+            it.successHandler(oAuth2LoginSuccessHandler)
         }
-
-         */
 
         // JWT 인증 필터
         http.addFilterBefore(JwtAuthenticationFilter(authTokenPort), UsernamePasswordAuthenticationFilter::class.java)
