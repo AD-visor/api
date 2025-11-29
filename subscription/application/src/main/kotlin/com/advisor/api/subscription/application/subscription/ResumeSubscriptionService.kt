de8616cf0adf77a5a4caf24e08f2e16dcddebcb3
liverforpresent <dailynsteady@gmail.com>
2025-11-30 00:07:06 +0900

    feat: 구독 재활성화 로직 구현

---
+++ subscription/application/src/main/kotlin/com/advisor/api/subscription/application/subscription/ResumeSubscriptionService.kt
@@ -0,0 +1,19 @@
+package com.advisor.api.subscription.application.subscription
+
+import com.advisor.api.common.core.domain.vo.identifier.SubscriptionId
+import com.advisor.api.subscription.domain.subscription.SubscriptionStore
+import com.advisor.api.subscription.port.inbound.subscription.ResumeSubscriptionUseCase
+import com.advisor.api.subscription.port.inbound.subscription.command.UpdateSubscriptionStatusCommand
+import org.springframework.stereotype.Service
+
+@Service
+class ResumeSubscriptionService(
+    private val subscriptionStore: SubscriptionStore
+): ResumeSubscriptionUseCase {
+    override fun execute(command: UpdateSubscriptionStatusCommand) {
+        val subscription = subscriptionStore.loadById(SubscriptionId(command.subscriptionId))
+        val updatedSubscription = subscription.resume()
+
+        subscriptionStore.save(updatedSubscription)
+    }
+}
