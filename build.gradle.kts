b43dc088b68b5f4c86ab1dabcab16c78946997cf
liverforpresent <dailynsteady@gmail.com>
2025-12-15 16:36:18 +0900

    chore: annotationProcessor -> kapt로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,9 @@ plugins {
 	kotlin("jvm") version "2.2.21"
 	kotlin("plugin.spring") version "2.2.21"
 	id("org.springframework.boot") version "4.0.0" apply false
-	id("io.spring.dependency-management") version "1.1.7" apply false
+	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "2.2.21" apply false
+	kotlin("kapt") version "2.2.21" apply false
 }
 
 group = "com.advisor"
@@ -28,35 +29,24 @@ subprojects {
 		}
 	}
 
-	configurations {
-		compileOnly {
-			extendsFrom(configurations.annotationProcessor.get())
+	dependencyManagement {
+		imports {
+			mavenBom("org.springframework.boot:spring-boot-dependencies:4.0.0")
+			mavenBom("org.springframework.ai:spring-ai-bom:1.1.2")
 		}
 	}
 
 	dependencies {
-		// Spring
-		implementation(platform("org.springframework.boot:spring-boot-dependencies:4.0.0"))
-		annotationProcessor(platform(("org.springframework.boot:spring-boot-dependencies:4.0.0")))
-
 		// Kotlin
 		implementation("tools.jackson.module:jackson-module-kotlin")
 		implementation("org.jetbrains.kotlin:kotlin-reflect")
 		implementation("io.github.microutils:kotlin-logging:3.0.5")
-
-		// Spring AI
-		implementation(platform("org.springframework.ai:spring-ai-bom:1.1.2"))
-
-		// Test
-		testImplementation("org.springframework.boot:spring-boot-starter-test")
-		testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-		testImplementation("org.springframework.security:spring-security-test")
-		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	}
 
 	kotlin {
 		compilerOptions {
-			freeCompilerArgs.addAll("-Xjsr305=strict")
+			jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+			freeCompilerArgs.add("-Xjsr305=strict")
 		}
 	}
 
