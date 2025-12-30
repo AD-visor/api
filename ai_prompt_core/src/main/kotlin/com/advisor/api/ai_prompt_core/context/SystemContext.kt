b91a9daa399e067093f986a02da639d7c3243da5
liverforpresent <dailynsteady@gmail.com>
2025-12-31 00:43:50 +0900

    feat: SystemContext 및 provider 구현

---
+++ ai_prompt_core/src/main/kotlin/com/advisor/api/ai_prompt_core/context/SystemContext.kt
@@ -0,0 +1,7 @@
+package com.advisor.api.ai_prompt_core.context
+
+data class SystemContext(
+    val identity: IdentityContext,
+    val reasoning: ReasoningContext,
+    val outputConstraint: OutputConstraintContext
+)
