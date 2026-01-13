ebdbf821682a74889e9f3b242fa2c087f3438e52
liverforpresent <dailynsteady@gmail.com>
2026-01-14 01:15:52 +0900

    feat: Media 인프라 예외 코드 정리

---
+++ media/adapter/outbound/src/main/kotlin/com/advisor/api/media/adapter/outbound/MediaInfrastructureExceptionCode.kt
@@ -0,0 +1,16 @@
+package com.advisor.api.media.adapter.outbound
+
+import com.advisor.api.common.exception.CustomExceptionCode
+import com.advisor.api.common.exception.ErrorStatus
+
+enum class MediaInfrastructureExceptionCode(
+    override val status: ErrorStatus,
+    override val code: String,
+    override val message: String
+): CustomExceptionCode {
+    MEDIA_NOT_FOUND(
+        status = ErrorStatus.NOT_FOUND,
+        code = "MEDIA_NOT_FOUND",
+        message = "[Media] 미디어를 찾을 수 없습니다."
+    ),
+}
