d39ab71b162fe2f07d10de7b2b164193cb0d8653
liverforpresent <dailynsteady@gmail.com>
2026-01-19 18:41:23 +0900

    feat: 미디어 삭제 로직 및 api 구현

---
+++ media/adapter/inbound/src/main/kotlin/com/advisor/api/media/adapter/inbound/MediaController.kt
@@ -0,0 +1,40 @@
+package com.advisor.api.media.adapter.inbound
+
+import com.advisor.api.common.core.presentation.BaseApiResponse
+import com.advisor.api.common.core.presentation.CustomUserDetails
+import com.advisor.api.media.port.inbound.DeleteMediaUseCase
+import com.advisor.api.media.port.inbound.command.DeleteMediaCommand
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/media")
+class MediaController(
+    private val deleteMediaUseCase: DeleteMediaUseCase
+) {
+    @DeleteMapping("/{mediaId}")
+    fun deleteMedia(
+        @AuthenticationPrincipal member: CustomUserDetails,
+        @PathVariable("mediaId") mediaId: String
+    ): ResponseEntity<BaseApiResponse<Unit>> {
+        val command = DeleteMediaCommand(
+            mediaId = mediaId.toLong(),
+            memberId = member.id
+        )
+
+        deleteMediaUseCase.execute(command)
+
+        val apiResponse = BaseApiResponse<Unit>(
+            success = true,
+            message = "Media deleted successfully",
+            httpStatus = HttpStatus.NO_CONTENT
+        )
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(apiResponse)
+    }
+}
