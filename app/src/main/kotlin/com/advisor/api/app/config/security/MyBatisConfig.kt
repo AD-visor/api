426024284a469d49cf990aea9a7742a54ea7a27f
liverforpresent <dailynsteady@gmail.com>
2026-04-14 21:17:34 +0900

    chore: 사용하지 않는 파일 및 의존성 삭제

--- app/src/main/kotlin/com/advisor/api/app/config/security/MyBatisConfig.kt
+++
@@ -1,4 +0,0 @@
-package com.advisor.api.app.config.security
-
-class MyBatisConfig {
-}
--- common/src/main/kotlin/com/advisor/api/common/core/domain/vo/identifier/ContentId.kt
+++
@@ -1,5 +0,0 @@
-package com.advisor.api.common.core.domain.vo.identifier
-
-import com.advisor.api.common.core.domain.vo.Identifier
-
-class ContentId(value: Long): Identifier<Long>(value)
